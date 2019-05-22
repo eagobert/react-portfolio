05949066869506be666eaaec2b98b3bbad3b2481
eagobert <eagobert@hotmail.com>
2019-05-21 21:43:11 -0500

    update fonts

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import Nav from './Navbar/Nav';
 import NavRoutes from './Navbar/NavRoutes';
 import './App.css';
+require('typeface-josefin-sans')
+require('typeface-lato')
+require('typeface-open-sans-condensed')
 
 class App extends Component {
   render() {
     return (
 
-        <div className="App">
+        <div className="App container-fluid">
 
           <header className="App-header">
             <Nav/>
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import './Home.css';
 
 class Home extends Component {
   render(){
     return(
       <React.Fragment>
-        <p>This is the home page</p>
+        <div className="nameBox">
+          <div id="myName">
+            <span className="first">ERIN</span>
+            <span className="last">AGOBERT</span>
+          </div>
+          <div id="myTitle">
+            <span className="title">Full-Stack Software Developer</span>
+          </div>
+
+        </div>
         
       </React.Fragment>
     );
