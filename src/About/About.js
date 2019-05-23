5155a1e4e523e4cb602c3b91eb24d2a120da1cb9
eagobert <eagobert@hotmail.com>
2019-05-23 06:33:49 -0500

    Add additional styling to about

--- src/About/About.js
+++ src/About/About.js
@@ -5,13 +5,13 @@ class About extends Component {
   render(){
     return(
       <React.Fragment>
-        <div className="col"></div>
-        <div id="aboutBox" className="col-lg-9">
+        {/* <div className="col"></div> */}
+        <div id="aboutBox" className="d-flex flex-column align-self-center">
           <p className="lineOne">Morty, I'm a drunk, not a hack. Come on, flip the pickle, Morty.</p>
           <p className="lineTwo">You're not gonna regret it. The payoff is huge.  Oh, that's Million </p>
           <p className="lineThree">Ants.  I can't see the ants from over here, I just assumed that </p>
         </div>
-        <div className="col"></div>
+        {/* <div className="col"></div> */}
       </React.Fragment>
     );
   }
