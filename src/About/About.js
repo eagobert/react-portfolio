3206a2480010e6670890b4f9618484af76e50d96
eagobert <eagobert@hotmail.com>
2019-05-23 06:08:32 -0500

    Add text and styling to about page

--- src/About/About.js
+++ src/About/About.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import './About.css';
 
 class About extends Component {
   render(){
     return(
       <React.Fragment>
-        <p>This is the about page.</p>
+        <div className="col"></div>
+        <div id="aboutBox" className="col-lg-9">
+          <p className="lineOne">Morty, I'm a drunk, not a hack. Come on, flip the pickle, Morty.</p>
+          <p className="lineTwo">You're not gonna regret it. The payoff is huge.  Oh, that's Million </p>
+          <p className="lineThree">Ants.  I can't see the ants from over here, I just assumed that </p>
+        </div>
+        <div className="col"></div>
       </React.Fragment>
     );
   }
