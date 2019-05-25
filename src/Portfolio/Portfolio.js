d286090db95a5f4a93f5859470042e1b97126def
eagobert <eagobert@hotmail.com>
2019-05-24 23:35:11 -0500

    Add project cards

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
+import './Portfolio.css';
+import comingsoon from '../Assets/ComingSoon.png'; 
 
 class Portfolio extends Component {
   render(){
     return(
       <React.Fragment>
-        <p>
-        This is the portfolio page
-        </p>
+        <div className="portfolioBox">
+          <div className="card-deck">
+            <div className="card">
+              <img className="card-img-top" src={comingsoon} alt="Coming Soon"></img>
+              <div className="card-body">
+                <h5 className="card-title">Lorem ipsum dolor sit amet, </h5>
+                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vestibulum mauris ut diam vulputate, nec scelerisque magna maximus. Suspendisse sit amet ex vestibulum, semper nunc quis, consequat arcu. Pellentesque feugiat molestie enim a aliquam. </p>
+              </div>
+            </div>
+            <div className="card">
+              <img className="card-img-top" src={comingsoon} alt="Coming Soon"></img>
+              <div className="card-body">
+                <h5 className="card-title">Lorem ipsum dolor sit amet, </h5>
+                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vestibulum mauris ut diam vulputate, nec scelerisque magna maximus. Suspendisse sit amet ex vestibulum, semper nunc quis, consequat arcu. Pellentesque feugiat molestie enim a aliquam. </p>
+              </div>
+            </div>
+            <div className="card">
+              <img className="card-img-top" src={comingsoon} alt="Coming Soon"></img>
+              <div className="card-body">
+                <h5 className="card-title">Lorem ipsum dolor sit amet, </h5>
+                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vestibulum mauris ut diam vulputate, nec scelerisque magna maximus. Suspendisse sit amet ex vestibulum, semper nunc quis, consequat arcu. Pellentesque feugiat molestie enim a aliquam. </p>
+              </div>
+            </div>
+
+          </div>
+        </div>
       </React.Fragment>
     );
   }
