02449b2354ee673f580f11398a35e1c2f18841d1
eagobert <eagobert@hotmail.com>
2019-05-25 00:03:00 -0500

    adjust card positioning

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -9,21 +9,21 @@ class Portfolio extends Component {
         <div className="portfolioBox">
           <div className="card-deck">
             <div className="card">
-              <img className="card-img-top" src={comingsoon} alt="Coming Soon"></img>
+              <img className="card-img-top w-75 align-self-center" src={comingsoon} alt="Coming Soon"></img>
               <div className="card-body">
                 <h5 className="card-title">Lorem ipsum dolor sit amet, </h5>
                 <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vestibulum mauris ut diam vulputate, nec scelerisque magna maximus. Suspendisse sit amet ex vestibulum, semper nunc quis, consequat arcu. Pellentesque feugiat molestie enim a aliquam. </p>
               </div>
             </div>
             <div className="card">
-              <img className="card-img-top" src={comingsoon} alt="Coming Soon"></img>
+              <img className="card-img-top w-75 align-self-center" src={comingsoon} alt="Coming Soon"></img>
               <div className="card-body">
                 <h5 className="card-title">Lorem ipsum dolor sit amet, </h5>
                 <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vestibulum mauris ut diam vulputate, nec scelerisque magna maximus. Suspendisse sit amet ex vestibulum, semper nunc quis, consequat arcu. Pellentesque feugiat molestie enim a aliquam. </p>
               </div>
             </div>
             <div className="card">
-              <img className="card-img-top" src={comingsoon} alt="Coming Soon"></img>
+              <img className="card-img-top w-75 align-self-center" src={comingsoon} alt="Coming Soon"></img>
               <div className="card-body">
                 <h5 className="card-title">Lorem ipsum dolor sit amet, </h5>
                 <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vestibulum mauris ut diam vulputate, nec scelerisque magna maximus. Suspendisse sit amet ex vestibulum, semper nunc quis, consequat arcu. Pellentesque feugiat molestie enim a aliquam. </p>
